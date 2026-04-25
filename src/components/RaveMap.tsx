7fcdfafb36cd650da6093c41460cd272723a9894
lukasort <37515581+lukasort@users.noreply.github.com>
2026-04-25 11:16:14 +0200

    add a toast like message when geolocation api is not available

--- src/components/RaveMap.tsx
+++ src/components/RaveMap.tsx
@@ -4,6 +4,7 @@ import { useTheme } from '../hooks/use-theme';
 import { getUserLocation } from '../helpers/userLocation';
 import { useEffect, useState } from 'react';
 import { icon } from 'leaflet';
+import { Toast } from './ui/toast';
 
 // Default center (Berlin) if geolocation fails or is denied
 const DEFAULT_CENTER: [number, number] = [52.52, 13.4050];
@@ -22,6 +23,7 @@ const RaveMap = () => {
     const { items } = useInfiniteList();
     const { resolvedTheme } = useTheme();
     const [userLocation, setUserLocation] = useState<GeolocationPosition | null>(null);
+    const [geoError, setGeoError] = useState<string | null>(null);
 
     useEffect(() => {
         getUserLocation()
@@ -30,6 +32,17 @@ const RaveMap = () => {
             })
             .catch(err => {
                 console.warn('Geolocation failed, using default center:', err);
+                if (err instanceof GeolocationPositionError) {
+                    if (err.code === err.PERMISSION_DENIED) {
+                        setGeoError('Geolocation is currently not available. Please enable location access in your browser settings.');
+                    } else if (err.code === err.POSITION_UNAVAILABLE) {
+                        setGeoError('Geolocation is currently not available. Location information is unavailable.');
+                    } else {
+                        setGeoError('Geolocation is currently not available. Request timed out.');
+                    }
+                } else {
+                    setGeoError('Geolocation is currently not available.');
+                }
             });
     }, []);
 
@@ -48,39 +61,49 @@ const RaveMap = () => {
         : DEFAULT_CENTER;
 
     return (
-        <MapContainer center={center}
-                      zoom={13}
-                      scrollWheelZoom={true}
-                      zoomControl={false}
-                      style={{ height: '100vh', width: '100vw' }}
-        >
-            {/* Update map view when userLocation changes */}
-            <MapCenterUpdater center={center} />
+        <>
+            {geoError && (
+                <Toast
+                    message={geoError}
+                    variant="destructive"
+                    duration={6000}
+                    onClose={() => setGeoError(null)}
+                />
+            )}
+            <MapContainer center={center}
+                          zoom={13}
+                          scrollWheelZoom={true}
+                          zoomControl={false}
+                          style={{ height: '100vh', width: '100vw' }}
+            >
+                {/* Update map view when userLocation changes */}
+                <MapCenterUpdater center={center} />
 
-            {/* Move zoom controls to bottom-right to avoid sidebar overlap */}
-            <ZoomControl position="bottomright" />
-            
-            <TileLayer
-                key={resolvedTheme}
-                attribution={
-                    resolvedTheme === 'dark'
-                        ? '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                        : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }
-                url={
-                    resolvedTheme === 'dark'
-                        ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
-                        : 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
-                }
-            />
-            {items.map((marker, index) => (
-                <Marker key={index} position={[marker.latitude, marker.longitude]} icon={customMarkerIcon} eventHandlers={{ click: () => handleMarkerClick(index) }}>
-                    <Popup>
-                        <span dangerouslySetInnerHTML={{ __html: marker.description }} />
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+                {/* Move zoom controls to bottom-right to avoid sidebar overlap */}
+                <ZoomControl position="bottomright" />
+                
+                <TileLayer
+                    key={resolvedTheme}
+                    attribution={
+                        resolvedTheme === 'dark'
+                            ? '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                            : '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    }
+                    url={
+                        resolvedTheme === 'dark'
+                            ? 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+                            : 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+                    }
+                />
+                {items.map((marker, index) => (
+                    <Marker key={index} position={[marker.latitude, marker.longitude]} icon={customMarkerIcon} eventHandlers={{ click: () => handleMarkerClick(index) }}>
+                        <Popup>
+                            <span dangerouslySetInnerHTML={{ __html: marker.description }} />
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </>
     );
 }
 
